63382eee3cb958aad4c7c345cad29482900ea594
Khanx Kenovis <maximuskhanx@hotmail.com>
2021-05-25 18:42:05 +0200

    /belong_empire <player_name>

--- Imperium/Commands/Belong.cs
+++ Imperium/Commands/Belong.cs
@@ -1,5 +1,6 @@
 ﻿using Chatting;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace Imperium.Commands
 {
@@ -8,11 +9,44 @@ namespace Imperium.Commands
     {
         public bool TryDoCommand(Players.Player player, string chat, List<string> splits)
         {
-            if (!chat.Trim().ToLower().Equals("/belong_empire"))
+            if (!splits[0].ToLower().Equals("/belong_empire"))
                 return false;
 
+            if (splits.Count == 1)
+            {
+                EmpireMenu.SendMenuBelong(player);
+            }
+            else
+            {
+                var m = Regex.Match(chat, @"/belong_empire( (?<targetplayername>['].+[']|[^ ]+))?");
 
-            EmpireMenu.SendMenuBelong(player);
+                if (!m.Success)
+                {
+                    Chatting.Chat.Send(player, "Syntax: /belong_empire [targetplayername]");
+                    return true;
+                }
+
+                string targetPlayerName = m.Groups["targetplayername"].Value;
+                Players.Player targetPlayer;
+                string error;
+
+                if (!PlayerHelper.TryGetPlayer(targetPlayerName, out targetPlayer, out error, true))
+                {
+                    Chatting.Chat.Send(player, $"Could not find '{targetPlayerName}'; {error}");
+                    return true;
+                }
+
+                Empire empire = Empire.GetEmpire(targetPlayer);
+
+                if(empire == null)
+                {
+                    Chatting.Chat.Send(player, $"'{targetPlayerName}' does not belong to any empire.");
+                }
+                else
+                {
+                    Chatting.Chat.Send(player, $"'{targetPlayerName}' belong to '{empire.name}'.");
+                }
+            }
 
             return true;
         }
